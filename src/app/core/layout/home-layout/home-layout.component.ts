8a8736080d725fbb5e3a9b50bbbb9a06faf6adee
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-05-19 21:30:14 +0300

    Add trusted

--- src/app/core/layout/home-layout/home-layout.component.ts
+++ src/app/core/layout/home-layout/home-layout.component.ts
@@ -3,10 +3,11 @@ import { NavbarComponent } from '../../../shared/components/ui/navbar/navbar.com
 import { FooterComponent } from "../../../shared/components/ui/footer/footer.component";
 import { DatePickerModule } from 'primeng/datepicker';
 import { FormsModule } from '@angular/forms';
+import { TrustedByComponent } from "../../../features/pages/trusted-by/trusted-by.component";
 
 @Component({
   selector: 'app-home-layout',
-  imports: [NavbarComponent, FooterComponent, DatePickerModule, FormsModule],
+  imports: [NavbarComponent, FooterComponent, DatePickerModule, FormsModule, TrustedByComponent],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.css'
 })
