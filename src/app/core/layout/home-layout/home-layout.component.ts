f96baf2da0a305a6dbdfd491780d4a53483411b9
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2025-05-23 01:13:34 +0300

    Special Gift

--- src/app/core/layout/home-layout/home-layout.component.ts
+++ src/app/core/layout/home-layout/home-layout.component.ts
@@ -3,10 +3,12 @@ import { NavbarComponent } from '../../../shared/components/ui/navbar/navbar.com
 import { FooterComponent } from "../../../shared/components/ui/footer/footer.component";
 import { DatePickerModule } from 'primeng/datepicker';
 import { FormsModule } from '@angular/forms';
+import { SpecialGiftsComponent } from "../../../features/pages/special-gifts/special-gifts.component";
+import { FeaturesComponent } from "../../../features/pages/features/features.component";
 
 @Component({
   selector: 'app-home-layout',
-  imports: [NavbarComponent, FooterComponent, DatePickerModule, FormsModule],
+  imports: [NavbarComponent, FooterComponent, DatePickerModule, FormsModule, SpecialGiftsComponent, FeaturesComponent],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.css'
 })
---
+++ src/app/features/pages/special-gifts/special-gifts.component.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
+interface ISpecialGift {
+  image: string,
+  title: string
+}
+interface ISpecial {
+  image: string,
+  name: string,
+  description: string,
+  btn: string
+}
+
+@Component({
+  selector: 'app-special-gifts',
+  imports: [CarouselModule],
+  templateUrl: './special-gifts.component.html',
+  styleUrl: './special-gifts.component.css'
+})
+export class SpecialGiftsComponent {
+  // Special Gift
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 500,
+    navText: ['<img src="../../../../assets/imgs/Special-gifts/left.png" class="w-[10px]" />', '<img src="../../../../assets/imgs/Special-gifts/right.png" class="w-[10px]" />'],
+    responsive: {
+      0: {
+        items: 1
+      }
+    },
+    nav: true
+  }
+
+  // SpecialGift
+  SpecialGift: ISpecialGift[] = [
+    {
+      image: '../../../../assets/imgs/Special-gifts/SpecialGift(1).jpg',
+      title: 'SpecialGift(1)'
+    },
+    {
+      image: '../../../../assets/imgs/Special-gifts/SpecialGift(2).jpg',
+      title: 'SpecialGift(2)'
+    },
+    {
+      image: '../../../../assets/imgs/Special-gifts/SpecialGift(3).jpg',
+      title: 'SpecialGift(3)'
+    },
+    {
+      image: '../../../../assets/imgs/Special-gifts/SpecialGift(4).jpg',
+      title: 'SpecialGift(4)'
+    }
+  ]
+
+
+  // Features
+  Special: ISpecial[] = [
+    {
+      image: '../../../../assets/imgs/Special-gifts/special-gift-1.png',
+      name: 'Gifts Box',
+      description: 'Awesome Gifts Box Collectons',
+      btn: 'Shop Now'
+    },
+    {
+      image: '../../../../assets/imgs/Special-gifts/special-gift-2.png',
+      name: 'Occasion Gifts',
+      description: 'Best Occasion Gifts Collections',
+      btn: 'Discover Now'
+    },
+    {
+      image: '../../../../assets/imgs/Special-gifts/special-gift-3.png',
+      name: 'Occasion Gifts',
+      description: 'Combo Sets Gift Box Up To 50% Off',
+      btn: 'Discover Now'
+    }
+  ]
+
+
+}
