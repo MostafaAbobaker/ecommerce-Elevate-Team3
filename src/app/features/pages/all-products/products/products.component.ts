6fe08bbdf757ae1bc5f936d84621c71760d27e58
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-06-22 01:18:55 +0300

    add style all products

--- src/app/features/pages/all-products/products/products.component.ts
+++ src/app/features/pages/all-products/products/products.component.ts
@@ -1,34 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../service/products.service';
-import { Subscription } from 'rxjs';
-import { IItemProduct } from '../../../../shared/components/ui/item-product/model/iitem-product';
-import { TranslatePipe } from '@ngx-translate/core';
-import { ItemProductComponent } from '../../../../shared/components/ui/item-product/item-product.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  imports: [TranslatePipe,ItemProductComponent],
+  imports: [],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent implements OnInit{
-    private getPopularItemsService!: Subscription;
-        productsItems:IItemProduct []  = [];
+export class ProductsComponent {
 
-  constructor(    private _productsService: ProductsService,
-){}
-  ngOnInit(): void {
-    this.getProducts()
-  }
-  getProducts() {
-    this.getPopularItemsService = this._productsService.getProducts().subscribe({
-        next: (res) => {
-          this.productsItems = res.products;
-        },
-        error: (err) => {
-          // this.messageService.add({ severity: 'error', summary: 'Error', detail: err.message });
-
-        },
-      });
-  }
 }
