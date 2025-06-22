116b1da4743a43a4b0fcb690424077ff33defa22
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-06-22 20:57:33 +0300

    Edit router lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,10 @@ import { HomePageComponent } from './features/pages/homePage/home-page/home-page
 export const routes: Routes = [
     {path:'', component:HomeLayoutComponent , children:[
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomePageComponent },
-      { path: 'all-Products', component: AllProductsComponent },
+      { path: 'home', loadChildren: () =>
+          import('./features/pages/homePage/home-page/home-page.component').then(m => m.HomePageComponent) },
+      { path: 'all-Products',  loadChildren: () =>
+          import('./features/pages/all-products/all-products.component').then(m => m.AllProductsComponent) },
 
     ]},
 
