58dfb0e06b46d1ac3ed564bd314d9d8474a83d00
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2025-06-24 22:29:12 +0300

    products ui

--- src/app/features/pages/all-products/products/products.component.ts
+++ src/app/features/pages/all-products/products/products.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ItemProductComponent } from "../../../../shared/components/ui/item-product/item-product.component";
+import { IItemProduct } from '../../../../shared/components/ui/item-product/model/iitem-product';
+import { TranslateModule } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
+import { ProductsService } from '../../homePage/popular-items/service/products.service';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ItemProductComponent, TranslateModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
+  providers: [MessageService]
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
+
+  productList: IItemProduct[] = [];
+  private getProductsService!: Subscription
+
+  constructor(
+    private _productsService: ProductsService,
+    private messageService: MessageService
+  ) { }
+
+  ngOnInit(): void {
+    this.getAllProducts()
+  }
+
+  getAllProducts() {
+    this.getProductsService = this._productsService.getAllCategories().subscribe({
+      next: (res) => {
+        this.productList = res.products;
+        console.log(res);
+
+      },
+      error: (err) => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: err.message });
+
+      },
+    });
+  }
+  ngOnDestroy(): void {
+    this.getProductsService.unsubscribe();
+  }
+
 
 }
