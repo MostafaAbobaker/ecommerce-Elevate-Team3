09df66924f0cca058737797292bb8f05246ff942
Muhamed-Elganzory <mohamedelganzory621@gmail.com>
2025-07-16 16:40:36 +0300

    feature/Auth_Library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import {Component, inject, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
-import {FormControl, FormGroup} from '@angular/forms';
-import {AuthLibService} from '../../projects/auth-lib/src/lib/auth-lib.service';
 
 @Component({
   selector: 'app-root',
@@ -10,31 +8,7 @@ import {AuthLibService} from '../../projects/auth-lib/src/lib/auth-lib.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'ecommerce-Elevate-Team5';
-
-
-  signInForm!: FormGroup;
-
-  private readonly authLibService: AuthLibService = inject(AuthLibService);
-
-  ngOnInit(): void {
-
-    this.signInForm = new FormGroup({
-      // resetCode: new FormControl('312985'),
-      email: new FormControl('rasidi4466@pacfut.com'),
-      newPassword: new FormControl('Rasidi1234')
-    });
-
-    this.authLibService.FORGET_PASSWORD(this.signInForm.value).subscribe({
-      next: (response): void => {
-        console.log('Sign In Response:', response);
-      },
-      error: (error): void => {
-        console.error('Sign In Error:', error);
-      }
-    })
-
-  }
 
 }
