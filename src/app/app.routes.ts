9bf95745a5ae8bb50d89ce6d7c284b67cdc1931b
Saied-AbdAlzaker <saiedabdalzaker@gmail.com>
2025-07-31 23:50:27 +0300

    edit path home in app-routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeLayoutComponent } from './core/layout/home-layout/home-layout.component';
 
 export const routes: Routes = [
   {path:'', component:HomeLayoutComponent , children:[
