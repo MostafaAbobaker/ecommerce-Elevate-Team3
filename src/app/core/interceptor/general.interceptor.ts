6757e37492a157a7d6b293c853b0f6a70d72966d
Muhamed-Elganzory <mohamedelganzory621@gmail.com>
2025-09-29 17:17:28 +0300

    Resolve-Conflict

--- src/app/core/interceptor/general.interceptor.ts
+++ src/app/core/interceptor/general.interceptor.ts
@@ -4,22 +4,18 @@ import { environment } from '../../../environments/environment';
 export const generalInterceptor: HttpInterceptorFn = (req, next) => {
   const baseUrl: string = environment.apiUrl;
 
-  // سيب الـ i18n requests زي ما هي
   if (req.url.includes('/i18n/')) {
     return next(req);
   }
 
-  // default headers
   let headers: Record<string, string> = {};
 
-  // Inject token بس لو احنا في المتصفح
   if (typeof window !== 'undefined' && localStorage) {
     const token = localStorage.getItem('token');
     if (token) {
       headers['Authorization'] = `Bearer ${token}`;
     }
 
-    // لو عايز كمان language header
     const lang: string = localStorage.getItem('lng') || 'en';
     headers['language'] = lang;
   }
