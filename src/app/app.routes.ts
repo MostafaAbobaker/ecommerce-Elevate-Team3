7d1408aa18e81df568b5f8d1a1dd0e85b28b6c29
MostafaAbobaker <35193789+MostafaAbobaker@users.noreply.github.com>
2025-10-13 11:11:59 +0300

    ed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,6 @@ export const routes: Routes = [
       { path: 'home', loadComponent: ()=> import('./features/pages/homePage/home-page/home-page.component').then (c => c.HomePageComponent) },
       { path: 'all-Products', loadComponent: ()=> import('./features/pages/all-products/all-products.component').then(c => c.AllProductsComponent) },
       { path: 'product-details/:id', loadComponent: ()=> import('./features/pages/all-products/product-details/product-details.component').then(c => c.ProductDetailsComponent) },
-<<<<<<< HEAD
       { path: 'orders', loadComponent: ()=> import('./features/pages/orders/orders.component').then(c => c.OrdersComponent) },
       { path: 'profile', loadComponent: ()=> import('./features/pages/profile/profile.component').then(c => c.ProfileComponent),children:[
 
@@ -19,9 +18,7 @@ export const routes: Routes = [
       ]
       },
 
-=======
       { path: 'cart', loadComponent: () => import('./features/pages/cart/cart.component').then(c => c.CartComponent), title: 'Cart', canActivate: [loggedGuard] },
->>>>>>> master
       /* { path: 'home', loadChildren: () =>
           import('./features/pages/homePage/home-page/home-page.component').then(m => m.HomePageComponent) },
       { path: 'all-Products',  loadChildren: () =>
